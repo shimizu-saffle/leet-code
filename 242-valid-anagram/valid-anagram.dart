5b7f0772b3956aba1e471439d5289df47a16c7ed
Kenta Shimizu <85660846+shimizu-saffle@users.noreply.github.com>
2023-12-15 21:02:44 +0900

    Time: 223 ms (87.62%) | Memory: 141.5 MB (75.25%) - LeetSync

--- 242-valid-anagram/valid-anagram.dart
+++ 242-valid-anagram/valid-anagram.dart
@@ -1,38 +1,17 @@
 class Solution {
   bool isAnagram(String s, String t) {
-    if (s.length != t.length) {
-      return false;
+    if (s.length != t.length) return false;
+
+    final counts = Map<int, int>();
+    for (final codePoint in s.runes) {
+      counts[codePoint] = (counts[codePoint] ?? 0) + 1;
     }
 
-    final sMap = Map<int, int>();
-    final sList = s.split('');
-    for (final e in sList) {
-      if (sMap[e.hashCode] == null) {
-        sMap[e.hashCode] = 1;
-      } else {
-        sMap[e.hashCode] = (sMap[e.hashCode] ?? 0) + 1;
-      }
-    }
-
-    final tMap = Map<int, int>();
-    final tList = t.split('');
-    for (final e in tList) {
-      if (tMap[e.hashCode] == null) {
-        tMap[e.hashCode] = 1;
-      } else {
-        tMap[e.hashCode] = (tMap[e.hashCode] ?? 0) + 1;
-      }
-    }
-
-    final sKeys = sMap.keys.toList();
-    final tKeys = tMap.keys.toList();
-    for (final sK in sKeys) {
-      if (!tKeys.contains(sK)) {
+    for (final codePoint in t.runes) {
+      if (!counts.containsKey(codePoint) || counts[codePoint] == 0) {
         return false;
       }
-      if (sMap[sK] != tMap[sK]) {
-        return false;
-      }
+      counts[codePoint] = counts[codePoint]! - 1;
     }
 
     return true;
