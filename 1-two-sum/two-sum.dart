8dc19a5eecbf3d51e1e9a40cf40b99d18c7b625a
Kenta Shimizu <85660846+shimizu-saffle@users.noreply.github.com>
2023-12-22 09:31:21 +0900

    Time: 355 ms (10.37%) | Memory: 152 MB (5.08%) - LeetSync

--- 1-two-sum/two-sum.dart
+++ 1-two-sum/two-sum.dart
@@ -1,21 +1,14 @@
 class Solution {
   List<int> twoSum(List<int> nums, int target) {
-    final result = <int>[];
-    for (final n in nums) {
-      final complement = target - n;
-      if (nums.contains(complement)) {
-        final currentIndex = nums.indexOf(n);
-        final duplicateIndex = nums.indexWhere((e) => n == e, currentIndex + 1);
-        if (duplicateIndex > 0) {
-          return [currentIndex, duplicateIndex];
-        }
-        final complementIndex = nums.indexOf(complement);
-        if (currentIndex != complementIndex) {
-          result.addAll([currentIndex, complementIndex]);
-          break;
-        }
+    final numsMap = <int, int>{};
+    for (int i = 0; i < nums.length; i++) {
+      final num = nums[i];
+      final complement = target - num;
+      if (numsMap.containsKey(complement)) {
+        return [numsMap[complement]!, i];
       }
+      numsMap[num] = i;
     }
-    return result;
+    throw Exception('No two sum solution found');
   }
 }
