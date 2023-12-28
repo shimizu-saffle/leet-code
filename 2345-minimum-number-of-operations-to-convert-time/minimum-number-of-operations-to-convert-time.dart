7edf8cd60db31fc7eb0b237d19c02b5ef54d2928
Kenta Shimizu <85660846+shimizu-saffle@users.noreply.github.com>
2023-12-28 21:14:56 +0900

    Time: 583 ms (100.00%) | Memory: 150.6 MB (100.00%) - LeetSync

--- 2345-minimum-number-of-operations-to-convert-time/minimum-number-of-operations-to-convert-time.dart
+++ 2345-minimum-number-of-operations-to-convert-time/minimum-number-of-operations-to-convert-time.dart
@@ -1,30 +1,20 @@
 class Solution {
   int convertTime(String current, String correct) {
-    final currentParts = current.split(':');
-    final correctParts = correct.split(':');
-
-    final currentHour = int.parse(currentParts[0]);
-    final currentMinute = int.parse(currentParts[1]);
-    final currentTotalMinute = (currentHour * 60) + currentMinute;
-
-    final correctHour = int.parse(correctParts[0]);
-    final correctMinute = int.parse(correctParts[1]);
-    final correctTotalMinute = (correctHour * 60) + correctMinute;
-
-    int gap = correctTotalMinute - currentTotalMinute;
-    var count = 0;
-    while (gap > 0) {
-      if (gap >= 60) {
-        gap = gap - 60;
-      } else if (gap >= 15) {
-        gap = gap - 15;
-      } else if (gap >= 5) {
-        gap = gap - 5;
-      } else if (gap >= 1) {
-        gap = gap - 1;
-      }
-      count++;
+    int timeToMinutes(String time) {
+      final parts = time.split(':');
+      return int.parse(parts[0]) * 60 + int.parse(parts[1]);
     }
+
+    int gap = timeToMinutes(correct) - timeToMinutes(current), count = 0;
+    const operations = [60, 15, 5, 1];
+
+    for (final op in operations) {
+      while (gap >= op) {
+        gap -= op;
+        count++;
+      }
+    }
+
     return count;
   }
 }
