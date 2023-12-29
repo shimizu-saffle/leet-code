a714f452e299d7e041cb9240dde80a5625ed365b
Kenta Shimizu <85660846+shimizu-saffle@users.noreply.github.com>
2023-12-29 13:18:24 +0900

    Time: 348 ms (50.00%) | Memory: 149.5 MB (50.00%) - LeetSync

--- 2345-minimum-number-of-operations-to-convert-time/minimum-number-of-operations-to-convert-time.dart
+++ 2345-minimum-number-of-operations-to-convert-time/minimum-number-of-operations-to-convert-time.dart
@@ -5,14 +5,13 @@ class Solution {
       return int.parse(parts[0]) * 60 + int.parse(parts[1]);
     }
 
-    int gap = timeToMinutes(correct) - timeToMinutes(current), count = 0;
+    int gap = timeToMinutes(correct) - timeToMinutes(current);
+    int count = 0;
     const operations = [60, 15, 5, 1];
 
     for (final op in operations) {
-      while (gap >= op) {
-        gap -= op;
-        count++;
-      }
+      count += gap ~/ op;
+      gap %= op;
     }
 
     return count;
