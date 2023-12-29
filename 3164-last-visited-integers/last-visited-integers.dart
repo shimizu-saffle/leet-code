997da84a41a3da7e36799c39b0e0cdfd898d7cf6
Kenta Shimizu <85660846+shimizu-saffle@users.noreply.github.com>
2023-12-29 16:42:18 +0900

    Time: 392 ms (100.00%) | Memory: 149.6 MB (100.00%) - LeetSync

--- 3164-last-visited-integers/last-visited-integers.dart
+++ 3164-last-visited-integers/last-visited-integers.dart
@@ -1,23 +1,21 @@
+import 'dart:collection';
+
 class Solution {
   List<int> lastVisitedIntegers(List<String> words) {
     int k = 0;
-    final visitedNumbers = <int>[];
+    final visitedNumbers = Queue<int>();
     final result = <int>[];
 
-    for (var i = 0; i < words.length; i++) {
-      final word = words[i];
-      if (words[i] == 'prev') {
+    words.asMap().forEach((i, word) {
+      if (word == 'prev') {
         k++;
-        if (k <= visitedNumbers.length) {
-          result.add(visitedNumbers[k - 1]);
-        } else {
-          result.add(-1);
-        }
+        result.add(
+            k <= visitedNumbers.length ? visitedNumbers.elementAt(k - 1) : -1);
       } else {
         k = 0;
-        visitedNumbers.insert(0, int.parse(word));
+        visitedNumbers.addFirst(int.parse(word));
       }
-    }
+    });
 
     return result;
   }
