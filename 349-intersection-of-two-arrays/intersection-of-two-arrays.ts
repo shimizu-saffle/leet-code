60bd699e07a66df2f484abbba6a0132b2eec0b3d
Kenta Shimizu <85660846+shimizu-saffle@users.noreply.github.com>
2023-12-31 10:50:24 +0900

    Time: 90 ms (5.26%) | Memory: 43.7 MB (99.69%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.ts
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.ts
@@ -1,4 +1,3 @@
 function intersection(nums1: number[], nums2: number[]): number[] {
-  const result = nums1.filter((e) => nums2.includes(e));
-  return Array.from(new Set(result));
+  return Array.from(new Set(nums1.filter((e) => nums2.includes(e))));
 }
