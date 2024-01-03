4c3e916662f2a3f902161d2c9f7f73d09e5f16e2
Kenta Shimizu <85660846+shimizu-saffle@users.noreply.github.com>
2024-01-03 13:55:29 +0900

    Time: 299 ms (100.00%) | Memory: 148.1 MB (100.00%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.dart
+++ 1127-last-stone-weight/last-stone-weight.dart
@@ -2,28 +2,22 @@ class Solution {
   int lastStoneWeight(List<int> stones) {
     if (stones.length < 2) return stones[0];
 
-    List<int> remainStones = [...stones]..sort((a, b) => b.compareTo(a));
+    stones.sort((a, b) => b.compareTo(a));
 
-    while (remainStones.length >= 2) {
-      int theHeaviestStone = 0;
-      int secondHeaviestStone = 0;
+    while (stones.length > 1) {
+      int stone1 = stones.removeAt(0);
+      int stone2 = stones.removeAt(0);
 
-      for (final stone in remainStones) {
-        if (theHeaviestStone < stone) {
-          theHeaviestStone = stone;
-        } else if (secondHeaviestStone < stone) {
-          secondHeaviestStone = stone;
+      if (stone1 != stone2) {
+        final newStone = stone1 - stone2;
+        int i = 0;
+        while (i < stones.length && stones[i] > newStone) {
+          i++;
         }
+        stones.insert(i, newStone);
       }
-
-      remainStones.remove(theHeaviestStone);
-      remainStones.remove(secondHeaviestStone);
-      remainStones.add(theHeaviestStone - secondHeaviestStone);
-      remainStones.sort((a, b) => b.compareTo(a));
-      theHeaviestStone = 0;
-      secondHeaviestStone = secondHeaviestStone = 0;
     }
 
-    return remainStones[0];
+    return stones.isEmpty ? 0 : stones[0];
   }
 }
