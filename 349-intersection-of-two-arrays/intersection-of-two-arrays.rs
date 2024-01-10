68dcc28bf93c7a2230b721e307f555b88478ccdb
Kenta Shimizu <85660846+shimizu-saffle@users.noreply.github.com>
2024-01-11 08:35:37 +0900

    Time: 1 ms (84.04%) | Memory: 2.3 MB (8.51%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.rs
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.rs
@@ -3,6 +3,11 @@ use std::collections::HashSet;
 impl Solution {
     pub fn intersection(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
         let nums2_set = nums2.into_iter().collect::<HashSet<i32>>();
-        nums1.into_iter().filter(|e| nums2_set.contains(e)).collect::<HashSet<i32>>().into_iter().collect()
+        nums1
+            .into_iter()
+            .filter(|e| nums2_set.contains(e))
+            .collect::<HashSet<i32>>()
+            .into_iter()
+            .collect()
     }
 }
