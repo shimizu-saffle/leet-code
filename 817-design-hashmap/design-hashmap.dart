0fe4052e3b66adcfb446ca92f22916a120abb8bc
Kenta Shimizu <85660846+shimizu-saffle@users.noreply.github.com>
2024-03-22 00:40:18 +0900

    Time: 423 ms (100.00%) | Memory: 180.8 MB (85.71%) - LeetSync

--- 817-design-hashmap/design-hashmap.dart
+++ 817-design-hashmap/design-hashmap.dart
@@ -1,33 +1,28 @@
-class Pair {
-  Pair(this.key, this.value);
-  final int key;
-  int value;
-}
-
 class MyHashMap {
-  MyHashMap();
-
   static const int _bucketSize = 1000;
-  final List<List<Pair>> buckets = List.generate(_bucketSize, (_) => <Pair>[]);
+  final List<List<(int key, int value)>> buckets =
+      List.generate(_bucketSize, (_) => []);
 
   void put(int key, int value) {
     final hash = key % _bucketSize;
     final bucket = buckets[hash];
-    for (var pair in bucket) {
-      if (pair.key == key) {
-        pair.value = value;
+    for (var i = 0; i < bucket.length; i++) {
+      var (pairKey, _) = bucket[i];
+      if (pairKey == key) {
+        bucket[i] = (key, value);
         return;
       }
     }
-    bucket.add(Pair(key, value));
+    bucket.add((key, value));
   }
 
   int get(int key) {
     final hash = key % _bucketSize;
     final bucket = buckets[hash];
     for (var pair in bucket) {
-      if (pair.key == key) {
-        return pair.value;
+      var (pairKey, pairValue) = pair;
+      if (pairKey == key) {
+        return pairValue;
       }
     }
     return -1;
@@ -36,6 +31,6 @@ class MyHashMap {
   void remove(int key) {
     final hash = key % _bucketSize;
     final bucket = buckets[hash];
-    bucket.removeWhere((pair) => pair.key == key);
+    bucket.removeWhere((pair) => pair.$1 == key);
   }
 }
